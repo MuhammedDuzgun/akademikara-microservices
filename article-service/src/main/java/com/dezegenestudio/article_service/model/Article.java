f7ef46e20c39183c7937c92b83dd82740f9e0197
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-10-20 02:47:17 +0300

    create Article model class

---
+++ article-service/src/main/java/com/dezegenestudio/article_service/model/Article.java
@@ -0,0 +1,16 @@
+package com.dezegenestudio.article_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Article {
+    private String title;
+    private String author;
+    private String date;
+}
