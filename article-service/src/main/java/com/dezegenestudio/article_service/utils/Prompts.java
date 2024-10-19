310ff244fff9c715e62d67f1cf48c1c18a614f05
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-10-20 02:54:47 +0300

    create Prompts class and add Article Prompt

---
+++ article-service/src/main/java/com/dezegenestudio/article_service/utils/Prompts.java
@@ -0,0 +1,5 @@
+package com.dezegenestudio.article_service.utils;
+
+public class Prompts {
+    public static final String articlePrompt = "Kullanıcıdan gelen konu hakkındaki en önemli 5 bilimsel makaleyi sıralayan bir asistansın";
+}
