561d11157bb37d8ebaa69c06d945b52b35974c18
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-10-21 02:09:09 +0300

    add save-article endpoint

--- article-service/src/main/java/com/dezegenestudio/article_service/controller/ArticleController.java
+++ article-service/src/main/java/com/dezegenestudio/article_service/controller/ArticleController.java
@@ -1,27 +1,34 @@
 package com.dezegenestudio.article_service.controller;
 
-import com.dezegenestudio.article_service.model.Article;
+import com.dezegenestudio.article_service.dto.SaveArticleDto;
+import com.dezegenestudio.article_service.entity.Article;
 import com.dezegenestudio.article_service.service.IArticleService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.dezegenestudio.article_service.service.ai.IArticleServiceAI;
+import com.dezegenestudio.article_service.service.impl.ArticleService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/article")
 public class ArticleController {
 
-    private final IArticleService articleService;
-
-    public ArticleController(IArticleService articleService) {
-        this.articleService = articleService;
-    }
+    private final IArticleServiceAI articleServiceAI;
+    private final ArticleService articleService;
 
     @GetMapping
     public List<Article> getRelatedArticles(@RequestParam String subject) {
-        return articleService.getRelatedArticles(subject);
+        return articleServiceAI.getRelatedArticles(subject);
+    }
+
+    @PostMapping("/save-article")
+    public ResponseEntity<SaveArticleDto> addArticle(@RequestBody SaveArticleDto articleDto) {
+        articleService.addArticle(articleDto);
+        return ResponseEntity.ok(articleDto);
     }
 
 }
